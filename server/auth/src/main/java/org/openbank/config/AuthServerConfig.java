cb86e7bc86a2622500f5d5f37a0bff89ca3b77a3
fyang1024 <fyang1024@gmail.com>
2019-02-16 22:41:25 +1100

    refactoring

--- server/auth/src/main/java/org/openbank/config/AuthServerConfig.java
+++ server/auth/src/main/java/org/openbank/config/AuthServerConfig.java
@@ -13,13 +13,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenEnhancer;
-import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableAuthorizationServer
